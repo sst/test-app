c8dd4e9a0f890a3149bd1f6782de665e4b973a6b
Dax Raad <d@ironbay.co>
2024-06-09 16:25:44 -0400

    init

---
+++ sst.config.ts
@@ -0,0 +1,12 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "test-app",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {},
+});
